89c8e535b8f2b48c1b75bb1a4d6aadaceb03a94a
Nicholas Draper <ndraper2@gmail.com>
2015-06-24 13:14:24 -0700

    set up the database session factory

--- journal.py
+++ journal.py
@@ -7,6 +7,10 @@ from waitress import serve
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
+from sqlalchemy.orm import scoped_session, sessionmaker
+from zope.sqlalchemy import ZopeTransactionExtension
+
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 
 Base = declarative_base()
 DATABASE_URL = os.environ.get(
@@ -48,10 +52,15 @@ def main():
     debug = os.environ.get('DEBUG', True)
     settings['reload_all'] = debug
     settings['debug_all'] = debug
+    if not os.environ.get('TESTING', False):
+        # only bind the session if we are not testing
+        engine = sa.create_engine(DATABASE_URL)
+        DBSession.configure(bind=engine)
     # configuration setup
     config = Configurator(
         settings=settings
     )
+    config.include('pyramid_tm')
     config.include('pyramid_jinja2')
     config.add_route('home', '/')
     config.add_route('other', '/other/{special_val}')
