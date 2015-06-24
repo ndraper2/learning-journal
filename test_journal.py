eaa56e04274b13a058e2bce781d458907194cd2c
Nicholas Draper <ndraper2@gmail.com>
2015-06-24 15:09:25 -0700

    additional tests for Entry.write, initial test for entry read

--- test_journal.py
+++ test_journal.py
@@ -62,3 +62,47 @@ def test_write_entry(db_session):
     # id and created should be set automatically upon writing to db:
     for auto in ['id', 'created']:
         assert getattr(entry, auto, None) is not None
+
+
+def test_write_entry_no_title(db_session):
+    bad_data = {'text': 'test text'}
+    journal.Entry.write(session=db_session, **bad_data)
+    with pytest.raises(IntegrityError):
+        db_session.flush()
+
+
+def test_entry_no_text(db_session):
+    bad_data = {'title': 'test_title'}
+    journal.Entry.write(session=db_session, **bad_data)
+    with pytest.raises(IntegrityError):
+        db_session.flush()
+
+
+def test_entry_extra_field(db_session):
+    bad_data = {'title': 'test title', 'text': 'test text', 'footer': 'test footer'}
+    with pytest.raises(TypeError):
+        journal.Entry.write(session=db_session, **bad_data)
+        db_session.flush()
+
+
+def test_read_entries_empty(db_session):
+    entries = journal.Entry.all()
+    assert len(entries) == 0
+
+
+def test_read_entries_one(db_session):
+    title_template = "Title {}"
+    text_template = "Entry Text {}"
+    # write three entries, with order clear in the title and text
+    for x in range(3):
+        journal.Entry.write(
+            title=title_template.format(x),
+            text=text_template.format(x),
+            session=db_session
+        )
+        db_session.flush()
+    entries = journal.Entry.all()
+    assert len(entries) == 3
+    assert entries[0].title > entries[1].title > entries[2].title
+    for entry in entries:
+        assert isinstance(entry, journal.Entry)
